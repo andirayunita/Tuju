8c134ae9c6ce2ec86228cf86fabc39dc68f8c35d
tommy Dwi Putranto <tommy.dwi@amarbank.co.id>
2022-08-28 10:13:40 +0700

    first commit

---
+++ project-7/project-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project-7
+//
+//  Created by Tommy-amarbank on 28/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
